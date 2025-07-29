1f5a5144d2e84c94a811c3f5563698de08be5f1c
Trevor <tm186833@gmail.com>
2025-07-29 10:45:24 +0100

    m

--- .vscode/Chess Visualization/script.js
+++ .vscode/Chess Visualization/script.js
@@ -1,12 +1,17 @@
+let board;
+
 function startTrainer() {
   document.getElementById("start-screen").style.display = "none";
   document.getElementById("trainer-screen").style.display = "block";
 
+  board = Chessboard('board', {
+    draggable: true,
+    position: 'start'
+  });
+
   // Hide notation after 5 seconds
   setTimeout(() => {
-    const notationBox = document.getElementById("notation-box");
-    if (notationBox) {
-      notationBox.style.display = "none";
-    }
+    document.getElementById("notation-box").style.display = "none";
   }, 5000);
 }
+
